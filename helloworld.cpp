6b8627eb9d2092f938dc3db83d27f743a7f5b46f
ShoweryCellar34 <126352167+ShoweryCellar34@users.noreply.github.com>
2023-07-31 17:11:47 +1200

    first commit

---
+++ helloworld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+
+    // Comment
+    
+    /*
+    Multiline Comment
+    */
+
+    std::cout << "Hello World!" << '\n';
+    std::cout << "Ok, I'm tired, go away world." << '\n';
+    return 0;
+}
