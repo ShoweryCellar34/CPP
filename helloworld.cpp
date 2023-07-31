1dfd6bf4d9f62eb88769f2e7d23f5e353a2b3638
ShoweryCellar34 <126352167+ShoweryCellar34@users.noreply.github.com>
2023-08-01 08:27:12 +1200

    modified:   helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -5,10 +5,35 @@ int main() {
     // Comment
     
     /*
-    Multiline Comment
+    Multi-line Comment
     */
 
+    // Interger (whole number)
+    int a = 5;
+    int sum = a + 6;
+
+    // Double (decimal point)
+    double b = 1.5;
+    double sum2 = b + 2.5;
+
+    // Single character
+    char c = 'A';
+
+    // Boolean (true of false)
+    bool d = true;
+
+    // String (can store a sequence of text)
+    std::string e = "ABC123!@#";
+
+    // '\n' is to start a new line
+    std::cout << sum << '\n';
     std::cout << "Hello World!" << '\n';
     std::cout << "Ok, I'm tired, go away world." << '\n';
+
+    // Const make varible, works with int, double, char, bool, string
+    const double PI = 3.14159;
+    double radius = 10;
+    double circumference = 2 * PI * radius;
+    std::cout << circumference << '\n';
     return 0;
 }
