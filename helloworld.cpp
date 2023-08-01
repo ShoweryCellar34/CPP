f130f5afbcd96eb717fa31b44952f719194287cb
ShoweryCellar34 <ShoweryCellar34@users.noreply.github.com>
2023-08-01 15:14:39 +1200

    modified:   helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -117,14 +117,40 @@ int main() {
         std::cout << "Number is less than 10." << '\n';
     }
 
-    // gets a interger and "case ..." runs if number is equal to ...
-    switch(number) { // 'number' is a interger that is used
+    // gets a varible and "case ..." segments runs if number is equal to ..., if no matching case then run defalt case (not mandatory)
+    switch(number) { // 'number' is a varible that is used
     case 1: // If 'number' is 1 then
         std::cout << "Number is 1!" << '\n';
         break;
     case 10: // If 'number' is 10 then
         std::cout << "Number is equal to 10!" << '\n';
         break;
+    default: // If no matching case then do ...
+        std::cout << "Number is not equal to 1 or 10." << '\n';
+    }
+
+    char character;
+    std::cout << "Enter a character: ";
+    std::cin >> character;
+
+    switch(character) { // Same as above but with letters and more cases
+        case 'A':
+            std::cout << "You entered 'A'" << '\n';
+            break;
+        case 'a':
+            std::cout << "You entered 'a'" << '\n';
+            break;
+        case 'B':
+            std::cout << "You entered 'B'" << '\n';
+            break;
+        case 'b':
+            std::cout << "You entered 'b'" << '\n';
+            break;
+        case 'C':
+            std::cout << "You entered 'c'" << '\n';
+            break;
+        default:
+            std::cout << "You did not enter a A, a, B, b, C or c." << '\n';
     }
 
     return 0;
