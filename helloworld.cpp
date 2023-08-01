344d688a85d9f5c3854e2b37315b6a78b5a3abfc
ShoweryCellar34 <ShoweryCellar34@users.noreply.github.com>
2023-08-01 14:45:44 +1200

    modified:   helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,76 +2,79 @@
 #include <cmath>
 
 // Createing a namespace
-namespace test {
+namespace example {
     int test = 21;
 }
 
 int main() {
 
     // Comment
-    
+
     /*
     Multi-line Comment
-
-    "typedef std::string nickname_t;" will allow access to std::string through nickname_t, _t is common to put when using typedef
-    "using nickname_t = std::string;" is more common and popular
-    should only use when clear benefit
-
-    Math operators: + - * /
-    "test += 1" increment by one
-    "test -= 1" decrement by one
-    "test *= 2" multiply by one
-    "test /= 2" divide by one
-    Order of calculation:
-        First: Parenthesis (Bedmas)
-        Second: Multiplication and Division
-        Third: Addition and Subtraction
-    Useful math functions:
-        Max and Min:
-            double x = 3;
-            double y = 4;
-            double z;
-            z = std::max(x, y); returns the bigger number of the selection
-            z = std::min(x, y); returns the smaller number of the selection
-        the rest need "#include <cmath>"
-        Power, Square root, absolute, rounding:
-        */
-            double z = pow(2, 4); // returns '16'
-            z = sqrt(9); // returns '3'
-            z = abs(-4); // returns '4'
-            z = round(3.14); // returns 3
-            z = ceil(3.14); // returns 4
-            z = floor(3.14); /* returns 3
-
-    Type conversion:
-        double x = (int) 3.14; 'x' will equal 3
-        int x = (double) 3.14; 'x' will equal 3
-        char x = 100; 'x' equals 'd' because the decimal representation of 'd' is 100 
-        std::cout << (char) 100; will print 'd'
     */
 
+    typedef std::string nickname_t; // will allow access to std::string through nickname_t, _t is common to put when using typedef
+    using nickname_t = std::string; // is more common and popular
+    //should only use when clear benefit
+
+    //Math operators: + - * /
+    int varible;
+    varible += 1; // increment by one
+    varible -= 1; // decrement by one
+    varible *= 2; // multiply by one
+    varible /= 2; // divide by one
+
+    //Order of calculation:
+    //First: Parenthesis (Bedmas)
+    //Second: Multiplication and Division
+    //Third: Addition and Subtraction
+
+    //Max and Min:
+    double a = 3;
+    double b = 4;
+    double c;
+    c = std::max(a, b); // returns the bigger number of the selection
+    c = std::min(a, b); // returns the smaller number of the selection
+        
+    //the rest need "#include <cmath>"
+    //Power, Square root, absolute, rounding:
+    double d = pow(2, 4); // returns '16'
+    d = sqrt(9); // returns '3'
+    d = abs(-4); // returns '4'
+    d = round(3.14); // returns 3
+    d = ceil(3.14); // returns 4
+    d = floor(3.14); // returns 3
+
     // Interger (whole number)
-    int test = 20;
-    int a = 5;
-    int sum = a + 6;
+    int e = 20;
+    int f = 5;
+    int sum = e + f;
 
     // Double (decimal point)
-    double b = 1.5;
-    double sum2 = b + 2.5;
+    double g = 1.5;
+    double h = 2.5;
+    double sum2 = g + h;
 
     // Single character
-    char c = 'A';
+    char i = 'A';
 
     // Boolean (true of false)
-    bool d = true;
+    bool j = true;
 
     // String (can store a sequence of text)
-    std::string e = "ABC123!@#";
+    std::string k = "ABC123!@#";
 
     // '\n' is to start a new line
     std::cout << sum << '\n';
     std::cout << "Hello World!" << '\n';
     std::cout << "Ok, I'm tired, go away world." << '\n';
+
+    // Type conversion:
+    double l = (int) 3.14; // 'l' will equal 3
+    int m = (double) 3.14; // 'm' will equal 3
+    char n = 100; // 'n' equals 'd' because the decimal representation of 'd' is 100 
+    std::cout << (char) 100; // will print 'd'
 
     // Const make varible, works with int, double, char, bool, string
     const double PI = 3.14159265359;
@@ -79,21 +82,22 @@ int main() {
     double circumference = 2 * PI * radius;
     std::cout << circumference << '\n';
 
-    // "using namespace test" would assume test namespace is default and no need to use "test::"
-    std::cout << test << ' ' << test::test << '\n';
+    // "using namespace test" would assume test namespace is default and no need to use "test::..."
+    int test = 20;
+    std::cout << test << ' ' << example::test << '\n';
     test++; // "test = test + 1"
-    std::cout << test << ' ' << test::test << '\n';
+    std::cout << test << ' ' << example::test << '\n';
     test--; // "test = test - 1"
-    std::cout << test << ' ' << test::test << '\n';
+    std::cout << test << ' ' << example::test << '\n';
 
     int remainder = 20 % 3; // Get the remainder of 20 / 3 using the modulus operator
     std::cout << remainder << '\n';
 
-    int x = 10;
-    int y = 8;
-    double z = x / y * 100; // 'z' will be 0
-    z = x / (double) y * 100; // 'z' will be 80
-    std::cout << z  << '%' << '\n';
+    int o = 10;
+    int p = 8;
+    double q = o / p * 100; // 'z' will be 0
+    q = o / (double) p * 100; // 'z' will be 80
+    std::cout << q  << '%' << '\n';
 
     std::string name;
     std::cout << "What is your full name: ";
@@ -104,15 +108,16 @@ int main() {
     std::cout << "Enter number: ";
     std::cin >> number;
 
-    if(number >= 10) {
+    // Only ever one set of instructions in the following statment will ever run
+    if(number >= 10) { // If number is greater or equal to ... than then do ...
         std::cout << "Number is greater than or equal to 10!" << '\n';
-    } else if(number < 0) {
+    } else if(number < 0) { // If nothing happend and number is less than ... than do ...
         std::cout << "Positive values only." << '\n';
-    } else {
+    } else { // If noting was triggered than do ...
         std::cout << "Number is less than 10." << '\n';
     }
-    return 0;
 
+    // gets a interger and "case ..." runs if number is equal to ...
     switch(number) { // 'number' is a interger that is used
     case 1: // If 'number' is 1 then
         std::cout << "Number is 1!" << '\n';
@@ -121,5 +126,7 @@ int main() {
         std::cout << "Number is equal to 10!" << '\n';
         break;
     }
+
+    return 0;
 
 }
